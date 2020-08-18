bd701c4e9aa910ca99f893c2ab44ae3c876507d6
sriramaravind <65369583+sriramaravind@users.noreply.github.com>
2020-08-19 00:52:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-app.use('/api', require('./routes/index'));
+app.use('/userserverapi3', require('./routes/index'));
 
 app.listen(app.get('port'));
 console.log('Server on port', app.get('port'));
